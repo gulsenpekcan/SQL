fb6f9bad9dc393f3bfea7ac82f155911179df01a
Gülşen Pekcan (ETIYA) <gulsen.pekcan@etiya.com>
2020-08-18 18:47:09 +0300

    duzenlendi

--- sql_parser_2.py
+++ sql_parser_2.py
@@ -7,16 +7,18 @@ from codecs import open
 def preprocess_query(query):
 
     # r'' yapisinda tirnak isaretleri icerisine yazilan \ (backslash) isaretleri yorumlanmaz
-    # satir atlama kisimlarini bosluk ile degistirir
-    query = query.replace('\n', ' ')
-    # verilen query icerisindeki .,:;=()- karakterlerini siler
-    query = re.sub(r'[.,:;=()-]', '', query)
-    # query'de yan yana 1 veya daha fazla bulunan | (pipe) karakterini kaldirir 
-    query = re.sub(r'[|]+', r'', query)
-    # query'deki ' karakterlerinin yerine bosluk koyar
-    query = re.sub(r'[\']+', r' ', query)
     # query'deki /*******/, /* comments */ yapilarini kaldirmak icin kullanilir
     query = re.sub(r'/\*.*?\*/', r'', query, flags = re.DOTALL)
+    # query'deki -- ile başlayan satirlari siler
+    query = re.sub(r'(?m)^[\s]*\-.*\n?', r'', query)
+    # verilen query icerisindeki .,:;=- karakterleri yerine bosluk koyar
+    query = re.sub(r'[.,:;=-]', ' ', query)
+    # query'de '|.....|' (pipe) karakterleri arasinda kalanlari kaldirir
+    query = re.sub(r'\'\|.*?\|\'', r'', query)
+    # query'deki ' karakterlerinin yerine bosluk koyar
+    query = re.sub(r'[\']+', r'', query)
+    # satir atlama kisimlarini bosluk ile degistirir
+    query = query.replace('\n', ' ')
 
     return query
 
@@ -25,29 +27,38 @@ def get_tables_names(query):
     # query'i bosluk karakterinden tokenlara ayirir
     tokens = re.split(r"[\s]+", query)
 
+    print(tokens)
     indices_join = []
     indices_from = []
-    
+    #indices_with = []
+
     for i in range(len(tokens)):
         if tokens[i] == 'JOIN' or tokens[i] == 'join':
             indices_join.append(i)
         elif tokens[i] == 'FROM' or tokens[i] == 'from':
             indices_from.append(i)
+        #elif tokens[i] == 'WITH' or tokens[i] == 'with':
+        #   indices_with.append(i)
 
     # tokenlarin icerisinden tablo isimlerini cekmeye yarayan bloklar
     tb_names = []
     for j in indices_join:
-        if(tokens[j+2][:1] != "_"):
-            tb_names.append(tokens[j+2])
+        if(tokens[j+1][:1] != "_"):
+            tb_names.append(tokens[j+1])
         else:
             pass
-        
+
     for j in indices_from:
-        if(tokens[j+2][:1] != "_"):
-            tb_names.append(tokens[j+2])
+        if(tokens[j+1][:1] != "_" and tokens[j+1][:1] != "("):
+            tb_names.append(tokens[j+1])
         else:
             pass
-    
+
+    """
+    for j in indices_with:
+        tb_names.append(tokens[j+1])
+    """
+
     # tablodaki elemanlarin tekligi kesinlestirilir
     tb_names = list(set(tb_names))
 
@@ -56,12 +67,15 @@ def get_tables_names(query):
 if __name__ == '__main__':
 
     # sql uzantili dosya okunup string olarak alinir
-    sqlfile = "C:/Users/gulsen.pekcan/Downloads/asset.sql"
+    #sqlfile = "C:/Users/gulsen.pekcan/Downloads/asset.sql"
+
+    sqlfile = "C:/Users/gulsen.pekcan/Downloads/cutomer.sql"
     sql_query = open(sqlfile, mode='r', encoding='utf-8-sig').read()
-    
+
+
     # string uzerinde on isleme gerceklestirilir
     sql_query = preprocess_query(sql_query)
-    
+
     # okunan sql uzantili dosyadaki tablo isimleri alinir
     names = []
     names = get_tables_names(sql_query)
@@ -70,4 +84,3 @@ if __name__ == '__main__':
     print( names)
     print("*******************************************************************")
 
-    
